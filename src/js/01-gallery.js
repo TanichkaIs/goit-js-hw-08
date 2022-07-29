a4e76314ce1c8a28bf75b9695cde41b4586a1b7f
TanichkaIs <93533516+TanichkaIs@users.noreply.github.com>
2022-07-29 14:07:49 +0200

    update 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,12 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+//import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилейт
+//import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+
+
 
 console.log(galleryItems);
